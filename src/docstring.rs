d546ca754d203ddee4098d53d6494574903948a6
wilhelmagren <wilhelmagren98@gmail.com>
2023-10-03 21:03:27 +0200

    feat(cli): format docstring with previous dates

--- src/docstring.rs
+++ src/docstring.rs
@@ -25,6 +25,8 @@
 * Last updated: 2023-10-02
 */
 
+use crate::FileType;
+
 use log::info;
 
 use regex::Regex;
@@ -37,19 +39,34 @@ use std::path::{Path, PathBuf};
 pub struct Docstring {
     target_path: PathBuf,
     license_path: PathBuf,
+    file_type: FileType,
     contents: Option<String>,
+    formatted_contents: Option<String>,
     file_created: Option<String>,
 }
 
 ///
 impl Docstring {
     ///
-    pub fn new(target: PathBuf, license: PathBuf) -> Self {
+    pub fn new(target: PathBuf, license: PathBuf, file_type: FileType) -> Self {
         Self {
             target_path: target,
             license_path: license,
+            file_type: file_type,
             contents: None,
+            formatted_contents: None,
             file_created: None,
+        }
+    }
+
+    ///
+    fn try_get_contents(&self) -> Result<String, io::Error> {
+        match &self.contents {
+            Some(c) => Ok(c.clone()),
+            None => Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "you have not read the contents of the LICENSE file yet",
+            )),
         }
     }
 
@@ -65,9 +82,12 @@ impl Docstring {
             Err(e) => return Err(e),
         };
 
-        let re = Regex::new(r"/^(\d{1,4})-(\d{1,2})-(\d{1,2})$/").expect("could not compile regex");
+        let re = Regex::new(r"\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*")
+            .expect("could not compile regex");
         for line in fc.split('\n') {
             if line.contains("File created: ") {
+                println!("{:?}", line);
+                println!("{:?}", re.find(line));
                 let date: String = match re.find(line) {
                     Some(d) => d.as_str().to_owned(),
                     None => {
@@ -78,10 +98,14 @@ impl Docstring {
                     }
                 };
                 self.file_created = Some(date);
+                return Ok(());
             };
         }
 
-        Ok(())
+        Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "could not find a created date",
+        ))
     }
 
     ///
@@ -98,6 +122,61 @@ impl Docstring {
             Err(e) => Err(e),
         }
     }
+
+    pub fn get_formatted_contents(self) -> Option<String> {
+        self.formatted_contents
+    }
+
+    ///
+    pub fn format_contents(&mut self) -> Result<(), io::Error> {
+        let contents = match self.try_get_contents() {
+            Ok(c) => c,
+            Err(e) => return Err(e),
+        };
+
+        let style = self.file_type.get_comment_style();
+        let start = style.start();
+        let comment = style.normal();
+        let end = style.end();
+
+        let mut formatted = String::new();
+
+        // start the multiline comment
+        formatted.push_str(start);
+        formatted.push('\n');
+
+        // add conents of the LICENSE file
+        for line in contents.split('\n') {
+            formatted.push_str(comment);
+            formatted.push_str(line);
+            formatted.push('\n');
+        }
+
+        // add created dates
+        formatted.push_str(comment);
+        formatted.push_str("File created: ");
+        let local: String = chrono::Local::now().format("%Y-%m-%d").to_string().clone();
+        let created = match &self.file_created {
+            Some(c) => c.as_str(),
+            None => local.as_str(),
+        };
+        formatted.push_str(created);
+        formatted.push('\n');
+
+        // add last updated date
+        formatted.push_str(comment);
+        formatted.push_str("Last updated: ");
+        formatted.push_str(local.as_str());
+        formatted.push('\n');
+
+        // end the multiline comment
+        formatted.push_str(end);
+        formatted.push_str("\n\n");
+
+        self.formatted_contents = Some(formatted);
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -105,5 +184,28 @@ mod tests_docstring {
     use super::*;
 
     #[test]
-    fn new_and_read() {}
+    fn read_license_ok() {
+        let target = PathBuf::from("src/docstring.rs");
+        let license = PathBuf::from("LICENSE");
+        let mut ds = Docstring::new(target, license, FileType::Rust);
+        ds.try_read_license().unwrap();
+    }
+
+    #[test]
+    fn find_created_date_ok() {
+        let target = PathBuf::from("src/docstring.rs");
+        let license = PathBuf::from("LICENSE");
+        let mut ds = Docstring::new(target, license, FileType::Rust);
+        ds.try_find_created_date().unwrap();
+    }
+
+    #[test]
+    fn format_contents_ok() {
+        let target = PathBuf::from("src/docstring.rs");
+        let license = PathBuf::from("LICENSE");
+        let mut ds = Docstring::new(target, license, FileType::Rust);
+        ds.try_read_license().unwrap();
+        ds.try_find_created_date().unwrap();
+        ds.format_contents().unwrap();
+    }
 }
